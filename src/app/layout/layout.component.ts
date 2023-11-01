17b460f226ebab4f972efc80fa30fc18c8c2e853
Taha Habib <80636271+TrojanPunk@users.noreply.github.com>
2023-11-01 10:20:57 +0530

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtDetails } from 'src/shared/models/interfaces/art-details';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  favorites: ArtDetails[] = [];
+
+  ngOnInit(): void {
+    localStorage.setItem('favorites', JSON.stringify(this.favorites));
+  }
 }
